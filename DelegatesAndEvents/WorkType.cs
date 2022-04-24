bf5031f784036e051103fff660299f350649dce3
brunocs90@gmail.com <brunocs90@gmail.com>
2022-04-24 15:40:24 -0300

    remove unnecessary usings

--- DelegatesAndEvents/WorkType.cs
+++ DelegatesAndEvents/WorkType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DelegatesAndEvents
+﻿namespace DelegatesAndEvents
 {
     public enum WorkType
     {
